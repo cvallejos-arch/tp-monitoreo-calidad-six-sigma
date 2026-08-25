a04793baf4b5c1c9d50fb44d57f8e9f5844f8782
martinpapaleo <martinpapaleo1@gmail.com>
2026-08-25 18:57:17 -0300

    c

---
+++ classes/defecto.py
@@ -0,0 +1,2 @@
+class Defecto:
+    def __init(self, tipo: str, descripcion: str, gravedad: int))
---
+++ validacinoes.py
@@ -0,0 +1,6 @@
+
+def validar_entero(numero):
+    return isinstance(numero, int)
+
+def validar_cantidad(cantidad):
+    return (va)
