b0498699739ff6e62bf57a2e882219a74c5c7078
martinpapaleo <martinpapaleo1@gmail.com>
2026-09-08 19:59:42 -0300

    modificaciones a Empresa

--- classes/empresa.py
+++ classes/empresa.py
@@ -9,22 +9,32 @@ class Empresa:
         self._procedimientos = []
         self._inspecciones = []
 
-    def registrar_lote(self, lote):
-        if isinstance(lote, Lote):
-            raise ValueError('El argumento debe ser instancia de Lote.')
+    def crear_registrar_lote(self):
+        lote = Lote()
         self._lotes.append(lote)
 
-    def registrar_muestra(self, muestra):
-        pass
+    def crear_registrar_muestra(self):
+        muestra = Muestra()
+        self._muestras.append(muestra)
 
-    def registrar_profesional(self, id, nombre):
-        pass
+    def crear_registrar_profesional(self):
+        profesional = Profesional()
+        self._profesionales.append(profesional)
 
-    def registrar_equipo(self, id, categoria, fecha_calibracion):
-        pass
+    def crear_registrar_equipo(self):
+        equipo = Equipo()
+        self._equipos.append(equipo)
 
-    def registrar_procedimiento(self, proc):
-        pass
+    def crear_registrar_procedimiento(self):
+        procedimiento = Procedimiento()
+        self._procedimientos.append(procedimiento)
+
+    def crear_registrar_inspeccion(self):
+        inspeccion = Inspeccion()
+        self._inspecciones.append(inspeccion)
+
+
+
 
     def lanzar_inspeccion(self, muestra, profesional, equipo, procedimiento, fecha):
         pass
@@ -33,3 +43,6 @@ class Empresa:
 if __name__ == "__main__":
     main()
 
+# dentro del main()
+Tech_2 = Empresa()
+Tech_2.registrar_lote
