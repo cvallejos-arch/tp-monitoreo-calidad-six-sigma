64792f9692b25fafbc9dcee86f561e386b772f98
ctronchetti <ctronchetti@itba.edu.ar>
2026-09-08 19:14:08 -0300

    mustra

--- classes/muestra.py
+++ classes/muestra.py
@@ -1,48 +1,116 @@
 from classes.estado_muestra import EstadoMuestra
-import uuid
+from classes.validacion import validar_texto_no_vacio, validar_entero_positivo
+from classes.excepciones import TransicionIlegalError
+from classes.reporte import Reporte
+
 
 class Muestra:
-    def __init__(self, cantidad, lote_id):
+    def __init__(self, id, cantidad):
         self._id = uuid.uuid4()
-        self._cantidad = cantidad
+        self._cantidad = validar_entero_positivo(cantidad, "cantidad")
         self._estado = EstadoMuestra.PENDIENTE
         self._defectos = []
-        self._lote_id = lote_id
+        self._lote_id = None
         self._inspeccion = None
         self._reporte = None
 
+    @property
     def get_id(self):
         return self._id
-
+    
+    @property
     def get_cantidad(self):
         return self._cantidad
-
+    
+    @property
     def get_estado(self):
         return self._estado
-
+    
+    @property
     def get_defectos(self):
         return self._defectos
-
+    
+    @property
     def get_lote_id(self):
         return self._lote_id
 
+    @property
+    def inspeccion(self):
+        return self._inspeccion
+
+    @property
     def get_reporte(self):
         return self._reporte
 
+#--- Gestion de la apartenencia al lote ---
+
     def asignar_lote(self, lote_id):
-        pass
+        if self._lote_id is not None:
+            raise TransicionIlegalError(
+                "La muestra '" + self._id + "' appartient déjà au lote "
+                "'" + self._lote_id + "' et ne peut pas être déplacée vers "
+                "'" + lote_id + "'."
+            )
+        self._lote_id = lote_id
 
     def iniciar_inspeccion(self):
-        pass
+        """ PENDIENTE a EN_INSPECCION"""
+        if self._estado != EstadoMuestra.PENDIENTE:
+            raise TransicionIlegalError(
+                "No se pudo iniciar la inspección: la muestra '" + self._id + 
+                "' está en estado" + self._estado.value + ", se espera PENDENTIE."
+            )
+        self._estado = EstadoMuestra.EN_INSPECCION
+        
 
     def agregar_defecto(self, defecto):
-        pass
+        """ anadir un defecto"""
+        if self._estado != EstadoMuestra.EN_INSPECCION:
+            raise TransicionIlegalError(
+                "No se pudo anadir un defecto: la muestra '" + self._id + 
+                "' está en estado" + self._estado.value + ", se espera EN_INSPECCION."
+            )
+        self._defectos.append(defecto)
 
     def cerrar(self, limite_gravedad):
-        pass
+        if self._estado != EstadoMuestra.EN_INSPECCION:
+            raise TransicionIlegalError(
+                "No se pudo cerrar la muestra '" + self._id + 
+                "' está en estado" + self._estado.value + ", se espera EN_INSPECCION."
+            )
+
+        if self.tiene_critico() or self.suma_gravedades() > limite_gravedad
+            self._estado = EstadoMuestra.NO_CONFORME
+        else:
+            self._estado = EstadoMuestra.CONFORME
+
+        if self._estado == EstadoMuestra.NO_CONFORME:
+            self._reporte = Reporte(
+                self._id,
+                self._lote_id,
+                self.inspeccion.profesional_id,
+                self._inspeccion.fecha,
+                self._defectos
+            )
+
 
     def suma_gravedades(self):
-        pass
+        total=0
+        for d in self._defectos:
+            total = total + d.gravedad
+        return total
+
 
     def tiene_critico(self):
-        pass
+        for d in self._defectos:
+            if d.es_critico():
+                return True
+        return False 
+
+    def __repr__(self):
+        return (
+            "Muestra(id=" + repr(self._id) + ", cantidad=" + str(self._cantidad) + ", "
+            "estado=" + self._estado.value + ", defectos=" + str(len(self._defectos)) + ", "
+            "lote_id=" + repr(self._lote_id) + ")"
+        )   
+            
