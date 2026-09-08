9d188aa70ac8ef02c104b82d8dc634a5475184ea
martinpapaleo <martinpapaleo1@gmail.com>
2026-09-08 17:56:06 -0300

    c

--- classes/validacion.py
+++ classes/validacion.py
@@ -16,13 +16,11 @@ def validar_fecha(fecha_str, formato="%d/%m/%Y"):
         # Si el formato o la fecha son inválidos
         return False
 
-print(validar_fecha('11/21/2024'))
-
 def validar_texto(texto, nombre_campo):
     if not isinstance(texto, str) or texto.strip() == "":
         raise ValueError(f"{nombre_campo} no puede estar vacío")
 
-
 def validar_rango_fechas(fecha_inicio, fecha_fin):
     if fecha_inicio > fecha_fin:
         raise ValueError("La fecha de inicio no puede ser posterior a la fecha de fin")
+    
