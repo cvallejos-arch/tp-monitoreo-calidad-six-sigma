683f010f9bb3ca6238305341c63c41a392bbd649
Martin <mpapeleo@itba.edu.ar>
2026-09-15 19:39:14 -0300

    cambios

--- classes/reporte.py
+++ classes/reporte.py
@@ -1,5 +1,9 @@
+import uuid
+
 class Reporte:
     def __init__(self, muestra_id, lote_id, profesional_id, fecha, defectos):
+        
+        self._id = uuid.uuid4()
         self._muestra_id = muestra_id
         self._lote_id = lote_id
         self._profesional_id = profesional_id
@@ -20,3 +24,5 @@ class Reporte:
 
     def get_defectos(self):
         return self._defectos
+
+
